591347e5baeb0156fbe7c0a1abf5c80201179e8b
Thomas Hayes <hayes.thomas610@gmail.com>
2022-08-05 12:08:44 -0600

    begin buildout of excrypt method and start helper methods for shifts

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,7 +2,20 @@ class Enigma
   def initialize
   end
 
-  def encryption()
+  def encrypt(message, key, date)
+    mod_message = message.downcase
+    encrypt_message = ""
+    alphabet_set = ("a".."z").to_a << " " 
+  
 
-  end 
+    mod_message.each_char.map do |character|
+      if alphabet_set.include?(character)
+        alpha_position = alphabet_set.index(character)
+        new_index = (alphabet_set + total_shift) % alphabet_set.count
+      
+      end
+    end
+  end
+
+  def build_keys_shift
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -8,19 +8,64 @@ RSpec.describe Enigma do
 
       expect(enigma).to be_instance_of(Enigma)
     end
-
+  end 
   describe '#encrypt' do
     it 'takes in argument(s) and returns a hash' do
     enigma.encrypt("hello world", "02715", "040895")
     
-    expected {
+    expected ({
           encryption: "keder ohulw",
           key: "02715",
           date: "040895"
-        }
+        })
 
       expect(enigma.encrypt("hello world", "02715", "040895")).to be_a(Hash)
       expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected)  
     end
   end
+
+  describe '#keys' do
+    xit 'splits up the key to find the shift' do
+      enigma.encrypt("hello world", "02715", "040895")
+
+      expected {
+        A: 02,
+        B: 27,
+        C: 71,
+        D: 15
+      }
+      
+      expect(enigma.encrypt.build_keys_shift).to eq(expected)
+    end
+  end 
+  
+  # describe '#offsets' do
+  #     xit 'generates a shift based on the provided date' do
+  #       enigma.encrypt("hello world", "02715", "040895")
+
+  #       expected {
+  #         A: 1,
+  #         B: 0,
+  #         C: 2,
+  #         D: 5
+  #       }
+
+  #       expect(enigma.encrypt.offsets).to eq(expected)
+  #     end
+  # end
+
+  # describe '#total_shift' do
+  #   xit 'returns the total shift based on the keys and offset' do
+  #       enigma.encrypt("hello world", "02715", "040895")
+
+  #       expected {
+  #         A: 3,
+  #         B: 27,
+  #         C: 73,
+  #         D: 20,
+  #       }
+
+  #       expect(enigma.excrypt.total_shift).to eq(expected)
+  #   end
+  # end
 end
